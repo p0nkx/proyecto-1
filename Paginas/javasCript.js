881d2cb6f0e10e570089d8e87bc35a08beae9774
p0nkx <lunafabianemmanuel@gmail.com>
2022-11-08 20:36:47 -0300

    agregando
    agrego el proyecto completo

---
+++ Paginas/javasCript.js
@@ -0,0 +1,22 @@
+
+    //imprime "hola mundo" y pregunta nombre y luego lo saluda
+       /* alert('hola mundo!');
+        let nombre=prompt('como te llamas?')
+        alert('Hola '+nombre);
+
+   alert(typeof nombre) devuelve el tipo de variable que es*/
+   /*var numero1=prompt('introduzca primer numero: ');
+   var numero2=prompt('introduzca segundo numero: ');
+   
+   var resultado=parseInt(numero1)+parseInt(numero2);//se convierte la el string en numero
+   alert(resultado)
+
+*/
+if (confirm ('Para acceder a este sitio usted debe tener 18 años o más,haga clic en "OK" si es el caso.')) 
+{
+alert ('Será redirigido al sitio.');
+}
+else 
+{
+alert ("Lo siento, usted no tiene acceso a esta página.");
+}
